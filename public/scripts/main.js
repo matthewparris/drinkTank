81a7acc23e0eb9c41e29214126879bc5a940bb4d
Michelle Chung <michka.chung@gmail.com>
2017-08-30 17:45:08 -0400

    styles being changed

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -1,5 +1,64 @@
-"use strict";
+'use strict';
 
 var ohHey = "Hello World";
 
 console.log(ohHey);
+
+var cocktailApp = {};
+
+cocktailApp.lcboApiKey = 'MDpkMDI1ZmE1Mi04YzM4LTExZTctOTEzNi04ZjUwZWNlZjYzZDk6YWc5TmJBN2pJcHJROFJSTWhMMHZQejVhbWJydVdUdHdWamdP';
+
+cocktailApp.lcboApiGetInventory = function (query) {
+	$.ajax({
+		url: 'https://lcboapi.com/products',
+		method: 'GET',
+		datatype: 'json',
+		data: {
+			per_page: 100,
+			q: query
+		},
+		headers: {
+			'Authorization': 'Token ' + cocktailApp.lcboApiKey
+		}
+	}).then(function (res) {
+		var inventoryItem = res.result;
+		console.log(inventoryItem);
+		cocktailApp.lcboApiDisplay(inventoryItem);
+	});
+};
+
+cocktailApp.lcboApiGetBoozeType = function () {
+	$('form.whatBooze').on('submit', function (e) {
+		e.preventDefault();
+		cocktailApp.lcboApiGetInventory($('input.inventoryChoice').val());
+	});
+};
+
+cocktailApp.lcboApiGetLocation = function (query) {
+	$.ajax({
+		url: 'https://lcboapi.com/stores',
+		method: 'GET',
+		datatype: 'json',
+		data: {
+			per_page: 100,
+			q: query
+		},
+		headers: {
+			'Authorization': 'Token ' + cocktailApp.lcboApiKey
+		}
+	}).then(function (res) {
+		var location = res.result;
+		console.log(location);
+		cocktailApp.lcboApiLocationDisplay(location);
+	});
+};
+
+cocktailApp.getLocation = function () {
+	$('form.place').on('submit', function (e) {
+		e.preventDefault();
+		cocktailApp.lcboApiGetLocation($('input.placeInput').val());
+	});
+};
+
+cocktailApp.getLocation();
+cocktailApp.lcboApiGetBoozeType();
